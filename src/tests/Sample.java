4cc15e51dce92176da5d1f457f82f0f7377004e6
Lokesh <lokesgo@gmail.com>
2020-01-05 12:42:30 +0000

    Changes UserName

--- src/tests/Sample.java
+++ src/tests/Sample.java
@@ -15,7 +15,7 @@ public class Sample {
 		
 		driver.get("https://www.facebook.com");
 		
-		driver.findElement(By.name("firstname")).sendKeys("Lokesh");
+		driver.findElement(By.name("firstname")).sendKeys("Koti");
 	}
 
 }
